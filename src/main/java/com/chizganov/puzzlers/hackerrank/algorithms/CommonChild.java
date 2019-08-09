04844545bba0e2e061d4782934908ee1b4529553
Ev Chizganov <ev@chizganov.com>
2019-08-09 23:20:11 +0700

    Fix comment

--- src/main/java/com/chizganov/puzzlers/hackerrank/algorithms/CommonChild.java
+++ src/main/java/com/chizganov/puzzlers/hackerrank/algorithms/CommonChild.java
@@ -1,6 +1,6 @@
 package com.chizganov.puzzlers.hackerrank.algorithms;
 
-/*
+/**
 * The longest common subsequence (LCS) problem is the problem of finding
 * the longest subsequence common to all sequences in a set of sequences
 * (often just two sequences).
