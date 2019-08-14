c6e5a7c3ed851f329fe6cf7e734589260a50a22a
Ev Chizganov <ev@chizganov.com>
2019-08-14 23:10:09 +0700

    Optimize The Bomberman Game challenge

--- src/main/java/com/chizganov/puzzlers/hackerrank/algorithms/TheBombermanGame.java
+++ src/main/java/com/chizganov/puzzlers/hackerrank/algorithms/TheBombermanGame.java
@@ -8,6 +8,8 @@ package com.chizganov.puzzlers.hackerrank.algorithms;
 class TheBombermanGame {
 
     String[] bomberMan(int n, String[] grid) {
+        if(n == 1) return grid;
+        // Every second second the field is full of bombs
         if(n % 2 == 0) {
             StringBuilder builder = new StringBuilder();
             for(int j = 0; j < grid[0].length(); j++) builder.append("O");
@@ -25,23 +27,27 @@ class TheBombermanGame {
                 if(grid[i].charAt(j) == 'O') a[i][j] = 1;
             }
         }
-
-        for(int i = 3; i <= n; i = i + 2) {
-            for(int r = 0; r < h; r++) {
+        // Every fourth second from 3 or 5 the same
+        // Like 3, 7, 11..
+        // Or 5, 9, 13..
+        int t = 3;
+        if((n-5) % 4 == 0) t = 5;
+        for(int i = 3; i <= t; i += 2) {
+            for (int r = 0; r < h; r++) {
                 for (int c = 0; c < w; c++) {
-                    if(a[r][c] == 0) a[r][c] = 3;
+                    if (a[r][c] == 0) a[r][c] = 3;
                 }
             }
 
-            for(int r = 0; r < h; r++) {
-                for(int c = 0; c < w; c++) {
-                    if(a[r][c] == 1) {
+            for (int r = 0; r < h; r++) {
+                for (int c = 0; c < w; c++) {
+                    if (a[r][c] == 1) {
                         a[r][c] = 0;
-                        if(r-1 >= 0) a[r-1][c] = 0;
-                        if(c-1 >= 0) a[r][c-1] = 0;
-                        if(r+1 < h && a[r+1][c] != 1) a[r+1][c] = 0;
-                        if(c+1 < w && a[r][c+1] != 1) a[r][c+1] = 0;
-                    } else if (a[r][c] == 3){
+                        if (r - 1 >= 0) a[r - 1][c] = 0;
+                        if (c - 1 >= 0) a[r][c - 1] = 0;
+                        if (r + 1 < h && a[r + 1][c] != 1) a[r + 1][c] = 0;
+                        if (c + 1 < w && a[r][c + 1] != 1) a[r][c + 1] = 0;
+                    } else if (a[r][c] == 3) {
                         a[r][c] = 1;
                     }
                 }
