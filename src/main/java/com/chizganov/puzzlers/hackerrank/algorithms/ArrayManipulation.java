334c0121da62b7f95eac4dd70b115cb5bdbe2bc2
Ev Chizganov <ev@chizganov.com>
2019-10-02 20:58:13 +0700

    Complete Array Manipulation challenge effectively

--- src/main/java/com/chizganov/puzzlers/hackerrank/algorithms/ArrayManipulation.java
+++ src/main/java/com/chizganov/puzzlers/hackerrank/algorithms/ArrayManipulation.java
@@ -9,17 +9,20 @@ class ArrayManipulation {
 
     long arrayManipulation(int n, int[][] queries) {
         long[] arr = new long[n];
-        long max = 0;
 
         for (int[] q : queries) {
-            int a = q[0];
-            int b = q[1];
+            int a = q[0] - 1;
+            int b = q[1] - 1;
             int k = q[2];
 
-            for (int i = a - 1; i < b; i++) {
-                arr[i] += k;
-                if (arr[i] > max) max = arr[i];
-            }
+            arr[a] += k;
+            if (b + 1 < n) arr[b + 1] += -k;
+        }
+
+        long max = arr[0];
+        for (int i = 1; i < n; i++) {
+            arr[i] += arr[i - 1];
+            if (arr[i] > max) max = arr[i];
         }
 
         return max;
