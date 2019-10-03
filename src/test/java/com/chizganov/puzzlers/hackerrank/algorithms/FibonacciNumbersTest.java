d8a06bd5aa6aebecaaf8bdb964e8b2d8d65bd04e
Ev Chizganov <ev@chizganov.com>
2019-10-03 23:59:08 +0700

    Complete Fibonacci numbers challenge

---
+++ src/test/java/com/chizganov/puzzlers/hackerrank/algorithms/FibonacciNumbersTest.java
@@ -0,0 +1,35 @@
+package com.chizganov.puzzlers.hackerrank.algorithms;
+
+import com.chizganov.puzzlers.util.TestSource;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+import static java.nio.file.Files.newInputStream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FibonacciNumbersTest {
+
+    private static FibonacciNumbers s = new FibonacciNumbers();
+
+    @ParameterizedTest
+    @TestSource(FibonacciNumbers.class)
+    void fibonacci(Path input, Path output) throws IOException {
+        InputStream inStream = newInputStream(input);
+        InputStream outStream = newInputStream(output);
+
+        try (Scanner in = new Scanner(inStream);
+             Scanner out = new Scanner(outStream)) {
+
+            int n = in.nextInt();
+
+            int actualResult = s.fibonacci(n);
+            int expectedResult = out.nextInt();
+            assertEquals(expectedResult, actualResult);
+        }
+    }
+
+}
