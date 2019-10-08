6b4d87f6af4df5e5f9c5b2c93b32cfa0f4fca2bd
Ev Chizganov <ev@chizganov.com>
2019-10-08 10:10:19 +0700

    Complete Sherlock and anagrams challenge

---
+++ src/test/java/com/chizganov/puzzlers/hackerrank/algorithms/SherlockAndAnagramsTest.java
@@ -0,0 +1,39 @@
+package com.chizganov.puzzlers.hackerrank.algorithms;
+
+import com.chizganov.puzzlers.util.TestSource;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+import static java.nio.file.Files.newInputStream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SherlockAndAnagramsTest {
+
+    private static final SherlockAndAnagrams s = new SherlockAndAnagrams();
+
+    @ParameterizedTest
+    @TestSource(SherlockAndAnagrams.class)
+    void test(Path input, Path output) throws IOException {
+        InputStream inStream = newInputStream(input);
+        InputStream expStream = newInputStream(output);
+
+        try (Scanner in = new Scanner(inStream);
+             Scanner exp = new Scanner(expStream)) {
+            int q = in.nextInt();
+            in.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int qItr = 0; qItr < q; qItr++) {
+                String l = in.nextLine();
+
+                int result = s.sherlockAndAnagrams(l);
+                int expResult = Integer.parseInt(exp.nextLine());
+                assertEquals(expResult, result);
+            }
+        }
+    }
+
+}
