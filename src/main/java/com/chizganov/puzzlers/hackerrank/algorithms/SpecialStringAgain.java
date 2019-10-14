c2eb131d4d535290c46a442dc8fef053efb894ce
Ev Chizganov <ev@chizganov.com>
2019-10-14 23:49:55 +0700

    Complete Special String Again challenge effectively

--- src/main/java/com/chizganov/puzzlers/hackerrank/algorithms/SpecialStringAgain.java
+++ src/main/java/com/chizganov/puzzlers/hackerrank/algorithms/SpecialStringAgain.java
@@ -1,34 +1,28 @@
 package com.chizganov.puzzlers.hackerrank.algorithms;
 
-import java.util.HashMap;
-import java.util.Map;
-
 class SpecialStringAgain {
 
     long substrCount(int n, String s) {
-        Map<String, Long> map = new HashMap<>();
-        for (int k = 1; k <= n; k++)
-            for (int i = 0; i <= n - k; i++) {
-                String sub = s.substring(i, i + k);
-                if (isValid(sub))
-                    map.merge(sub, 1L, Long::sum);
+        long count = n;
+        int mid = -1;
+        int eqSeq = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i - 1) == s.charAt(i)) {
+                count += eqSeq;
+                eqSeq++;
+                if ((mid - eqSeq >= 0) && s.charAt(mid - eqSeq) == s.charAt(i)) count++;
+                else mid = -1;
+            } else {
+                eqSeq = 1;
+                if ((i - 2 >= 0) && s.charAt(i - 2) == s.charAt(i)) {
+                    count++;
+                    mid = i - 1;
+                } else mid = -1;
             }
-
-        return map.values().stream().mapToLong(v -> v).sum();
-    }
-
-    private boolean isValid(String s) {
-        char[] chrs = s.toCharArray();
-        char expected = chrs[0];
-
-        int i;
-        for (i = 1; i < s.length() / 2; i++) {
-            if (chrs[i] != expected) return false;
         }
-        for (i = s.length() % 2 == 0 ? i : i + 1; i < s.length(); i++)
-            if (chrs[i] != expected) return false;
 
-        return true;
+        return count;
     }
 
 }
