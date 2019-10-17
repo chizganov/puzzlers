31d6a5e670c6fd03804680780ea9091a520d93b1
Ev Chizganov <ev@chizganov.com>
2019-10-17 14:01:06 +0700

    Complete Count triplets challenge

---
+++ src/test/java/com/chizganov/puzzlers/hackerrank/algorithms/CountTripletsTest.java
@@ -0,0 +1,44 @@
+package com.chizganov.puzzlers.hackerrank.algorithms;
+
+import com.chizganov.puzzlers.util.TestSource;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.nio.file.Files.newInputStream;
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CountTripletsTest {
+
+    private static CountTriplets s = new CountTriplets();
+
+    @ParameterizedTest
+    @TestSource(CountTriplets.class)
+    void countTriplets(Path input, Path output) throws IOException {
+        InputStream inStream = newInputStream(input);
+        InputStream outStream = newInputStream(output);
+
+        try (Scanner in = new Scanner(inStream);
+             Scanner out = new Scanner(outStream)) {
+            int n = in.nextInt();
+            int r = in.nextInt();
+            in.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            List<Long> arr = Arrays.stream(in.nextLine().split(" "))
+                    .mapToLong(Long::parseLong)
+                    .boxed()
+                    .collect(toList());
+
+            Long actualResult = s.countTriplets(arr, r);
+            Long expectedResult = out.nextLong();
+            assertEquals(expectedResult, actualResult);
+        }
+    }
+
+}
