561d62d173c09f817419be2ab15b67b5326e7033
Ev Chizganov <ev@chizganov.com>
2019-11-16 23:54:16 +0700

    Complete Queue Two Stacks challenge

---
+++ src/test/java/com/chizganov/puzzlers/hackerrank/algorithms/QueueTwoStacksTest.java
@@ -0,0 +1,50 @@
+package com.chizganov.puzzlers.hackerrank.algorithms;
+
+import com.chizganov.puzzlers.util.TestSource;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+import static java.nio.file.Files.newBufferedReader;
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class QueueTwoStacksTest {
+
+    @ParameterizedTest
+    @TestSource(QueueTwoStacks.class)
+    void testQueue(QueueTwoStacks<Integer> solution, Path input, Path output) throws IOException {
+        try (BufferedReader in = newBufferedReader(input);
+             BufferedReader out = newBufferedReader(output)) {
+            in.readLine();
+
+            List<String[]> queries = in.lines().map(s -> s.split(" ")).collect(toList());
+
+            List<String> resultList = new ArrayList<>();
+            for (String[] query : queries) {
+                switch (parseInt(query[0])) {
+                    case 1:
+                        solution.enqueue(parseInt(query[1]));
+                        break;
+                    case 2:
+                        solution.dequeue();
+                        break;
+                    case 3:
+                        Integer e = solution.peek();
+                        resultList.add(e.toString());
+                        break;
+                }
+            }
+
+            String[] expectedResult = out.lines().toArray(String[]::new);
+            String[] actualResult = resultList.toArray(new String[0]);
+            assertArrayEquals(expectedResult, actualResult);
+        }
+    }
+
+}
