a27253048c933119084fdc3c519bf0486b13d0a7
Ev Chizganov <ev@chizganov.com>
2019-12-22 23:54:46 +0700

    Complete Project Euler #1: Multiples of 3 and 5

---
+++ src/test/java/com/chizganov/puzzlers/hackerrank/euler/MultiplesOfThreeAndFiveTest.java
@@ -0,0 +1,29 @@
+package com.chizganov.puzzlers.hackerrank.euler;
+
+import com.chizganov.puzzlers.util.TestSource;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static java.nio.file.Files.newBufferedReader;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class MultiplesOfThreeAndFiveTest {
+
+    @ParameterizedTest
+    @TestSource(MultiplesOfThreeAndFive.class)
+    void multiples(MultiplesOfThreeAndFive solution, Path input, Path output) throws IOException {
+        try (BufferedReader in = newBufferedReader(input);
+             BufferedReader out = newBufferedReader(output)) {
+            in.readLine();
+
+            long[] expectedResult = out.lines().mapToLong(Integer::parseInt).toArray();
+            long[] actualResult = in.lines().mapToInt(Integer::parseInt).mapToLong(solution::multiples).toArray();
+
+            assertArrayEquals(expectedResult, actualResult);
+        }
+    }
+
+}
