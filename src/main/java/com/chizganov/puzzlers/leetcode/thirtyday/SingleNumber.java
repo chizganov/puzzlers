97a27ed3c2ad29eb4707587dddb8fc8c49ba18f7
Ev Chizganov <ev@chizganov.com>
2020-04-14 20:41:05 +0700

    Complete Single number challenge (30 day challenge)

---
+++ src/main/java/com/chizganov/puzzlers/leetcode/thirtyday/SingleNumber.java
@@ -0,0 +1,23 @@
+package com.chizganov.puzzlers.leetcode.thirtyday;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a href="https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3283/">Single Number challenge</a>
+ *
+ * @author Ev Chizganov
+ */
+class SingleNumber {
+
+    int singleNumber(int[] nums) {
+        Map<Integer, Integer> numToCount = new HashMap<>();
+        for (int n : nums) {
+            if (numToCount.containsKey(n)) numToCount.remove(n);
+            else numToCount.put(n, 1);
+        }
+
+        return numToCount.keySet().iterator().next();
+    }
+
+}
