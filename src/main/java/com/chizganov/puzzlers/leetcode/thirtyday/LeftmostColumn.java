824410d9a78b1c93858c09492abaecfe144df4e4
Ev Chizganov <ev@chizganov.com>
2020-04-22 05:44:33 +0700

    Complete Leftmost Column with at Least a One challenge (30 day challenge)

---
+++ src/main/java/com/chizganov/puzzlers/leetcode/thirtyday/LeftmostColumn.java
@@ -0,0 +1,57 @@
+package com.chizganov.puzzlers.leetcode.thirtyday;
+
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+/**
+ * <a href="https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/530/week-3/3306/">Leftmost Column with at Least a One challenge</a>
+ *
+ * @author Ev Chizganov
+ */
+class LeftmostColumn {
+
+    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        int HEIGHT = binaryMatrix.dimensions().get(0);
+        int WIDTH = binaryMatrix.dimensions().get(1);
+
+        int result = -1;
+
+        for (int i = 0; i < HEIGHT; i++) {
+            if (binaryMatrix.get(i, WIDTH - 1) == 1) {
+                int leftover = findLeftover(binaryMatrix, i, 0, WIDTH - 1);
+                if (leftover < result || result == -1) result = leftover;
+            }
+        }
+
+        return result;
+    }
+
+    private int findLeftover(BinaryMatrix bm, int row, int left, int right) {
+        if (left == right) return left;
+
+        int mid = (left + right) / 2;
+        return bm.get(row, mid) == 1 ? findLeftover(bm, row, left, mid) : findLeftover(bm, row, mid + 1, right);
+    }
+
+    static class BinaryMatrix {
+
+        private int[][] matrix;
+        private List<Integer> dimensions;
+
+        BinaryMatrix(int[][] matrix) {
+            this.matrix = matrix;
+            dimensions = Collections.unmodifiableList(asList(matrix.length, matrix[0].length));
+        }
+
+        int get(int x, int y) {
+            return matrix[x][y];
+        }
+
+        List<Integer> dimensions() {
+            return dimensions;
+        }
+    }
+
+}
