bb5c8c8e3ba39df93b7c8d5c6550353fa9515c0f
Ev Chizganov <ev@chizganov.com>
2020-05-04 19:14:25 +0700

    Simplify Number Complement problem solution

--- src/main/java/com/chizganov/puzzlers/leetcode/may/NumberComplement.java
+++ src/main/java/com/chizganov/puzzlers/leetcode/may/NumberComplement.java
@@ -1,5 +1,7 @@
 package com.chizganov.puzzlers.leetcode.may;
 
+import static java.lang.Integer.highestOneBit;
+
 /**
  * <a href="https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3319/">Number Complement problem</a>
  *
@@ -8,15 +10,7 @@ package com.chizganov.puzzlers.leetcode.may;
 class NumberComplement {
 
     int findComplement(int num) {
-        int mask = 1;
-
-        int highBitCount = 1;
-        for (int t = mask << 1, step = 1; t != 0x8000_0000; t <<= 1, step++) {
-            if ((num & t) != 0) highBitCount = step;
-        }
-        while (highBitCount-- > 1) mask = (mask << 1) | 1;
-
-        return ~num & mask;
+        return ~num + (highestOneBit(num) << 1);
     }
 
 }
