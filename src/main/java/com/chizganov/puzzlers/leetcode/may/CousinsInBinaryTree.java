f1e1a56d44cc39bb63c3ce15aabbf751f040e18d
Ev Chizganov <ev@chizganov.com>
2020-05-07 18:31:51 +0700

    Complete Cousins in Binary Tree problem (May challenge)

---
+++ src/main/java/com/chizganov/puzzlers/leetcode/may/CousinsInBinaryTree.java
@@ -0,0 +1,43 @@
+package com.chizganov.puzzlers.leetcode.may;
+
+import com.chizganov.puzzlers.leetcode.util.TreeNode;
+
+/**
+ * <a href="https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3322/">Cousins In Binary Tree challenge</a>
+ *
+ * @author Ev Chizganov
+ */
+class CousinsInBinaryTree {
+
+    boolean isCousins(TreeNode root, int x, int y) {
+        return !isSiblings(root, x, y) && depth(root, x, 0) == depth(root, y, 0);
+    }
+
+    private int depth(TreeNode node, int n, int curDepth) {
+        if (node == null) return -1;
+        if (node.getVal() == n) return curDepth;
+
+        int leftDepth = depth(node.getLeft(), n, curDepth + 1);
+        int rightDepth = depth(node.getRight(), n, curDepth + 1);
+
+        return leftDepth != -1 ? leftDepth : rightDepth;
+    }
+
+    private boolean isSiblings(TreeNode node, int x, int y) {
+        if (node == null) return false;
+
+        TreeNode left = node.getLeft();
+        TreeNode right = node.getRight();
+
+        if (containsVal(left, x) || containsVal(left, y)) {
+            return containsVal(right, x) || containsVal(right, y);
+        } else {
+            return isSiblings(left, x, y) || isSiblings(right, x, y);
+        }
+    }
+
+    private boolean containsVal(TreeNode node, int val) {
+        return node != null && node.getVal() == val;
+    }
+
+}
---
+++ src/test/java/com/chizganov/puzzlers/leetcode/may/CousinsInBinaryTreeTest.java
@@ -0,0 +1,35 @@
+package com.chizganov.puzzlers.leetcode.may;
+
+import com.chizganov.puzzlers.leetcode.util.TreeNode;
+import com.chizganov.puzzlers.util.TestSource;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static java.lang.Boolean.parseBoolean;
+import static java.lang.Integer.parseInt;
+import static java.nio.file.Files.newBufferedReader;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CousinsInBinaryTreeTest {
+
+    @ParameterizedTest
+    @TestSource(CousinsInBinaryTree.class)
+    void isCousins(CousinsInBinaryTree solution, Path input, Path output) throws IOException {
+        try (BufferedReader in = newBufferedReader(input);
+             BufferedReader out = newBufferedReader(output)) {
+            String btreeString = in.readLine();
+            TreeNode btree = TreeNode.createBinaryTreeFromBfsString(btreeString, ",", "null");
+            int x = parseInt(in.readLine());
+            int y = parseInt(in.readLine());
+
+            boolean expectedResult = parseBoolean(out.readLine());
+            boolean actualResult = solution.isCousins(btree, x, y);
+
+            assertEquals(expectedResult, actualResult,
+                    String.format("Input: tree [%s], x = %d, y = %d", btree, x, y));
+        }
+    }
+}
