b83fb8aed003018d3c5944b48e4a557bcca61c1f
Ev Chizganov <ev@chizganov.com>
2020-05-13 20:49:16 +0700

    Complete Remove K Digits problem (May challenge)

---
+++ src/main/java/com/chizganov/puzzlers/leetcode/may/RemoveDigits.java
@@ -0,0 +1,34 @@
+package com.chizganov.puzzlers.leetcode.may;
+
+/**
+ * <a href="https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3328/">Remove K Digits problem</a>
+ *
+ * @author Ev Chizganov
+ */
+class RemoveDigits {
+
+    String removeKdigits(String num, int k) {
+        int size = num.length() - k;
+        char[] buffer = new char[size];
+
+        for (int i = 0; i < size; i++) {
+            buffer[i] = num.charAt(k + i);
+        }
+        for (int i = k - 1; i >= 0; i--) {
+            char replacement = num.charAt(i);
+            int position = 0;
+            while (position < size && buffer[position] >= replacement) {
+                char t = buffer[position];
+                buffer[position] = replacement;
+                replacement = t;
+                position++;
+            }
+        }
+
+        int startIndex = 0;
+        while (startIndex < size && buffer[startIndex] == '0') startIndex++;
+
+        return startIndex != size ? String.valueOf(buffer, startIndex, size - startIndex) : "0";
+    }
+
+}
