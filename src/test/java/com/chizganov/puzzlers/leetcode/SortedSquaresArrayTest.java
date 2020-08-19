f7094029e64fd83a1a96cfc2443280b61d0043ed
Ev Chizganov <ev@chizganov.com>
2020-08-19 17:28:24 +0700

    Complete Squares of a Sorted Array challenge

---
+++ src/test/java/com/chizganov/puzzlers/leetcode/SortedSquaresArrayTest.java
@@ -0,0 +1,34 @@
+package com.chizganov.puzzlers.leetcode;
+
+import com.chizganov.puzzlers.util.TestSource;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+import static java.nio.file.Files.newBufferedReader;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class SortedSquaresArrayTest {
+
+    @ParameterizedTest
+    @TestSource(SortedSquaresArray.class)
+    void sortedSquares(SortedSquaresArray solution, Path input, Path output) throws IOException {
+        try (BufferedReader in = newBufferedReader(input);
+             BufferedReader out = newBufferedReader(output)) {
+            int[] inputArray = Arrays.stream(in.readLine().split(",")).mapToInt(Integer::parseInt).toArray();
+            int[] a = Arrays.copyOf(inputArray, inputArray.length);
+
+            int[] expectedResult = Arrays.stream(out.readLine().split(",")).mapToInt(Integer::parseInt).toArray();
+            int[] actualResult = solution.sortedSquares(a);
+
+            assertArrayEquals(expectedResult, actualResult,
+                    String.format("Input array: %s\nExpected result: %s\nActual result: %s\n",
+                            Arrays.toString(inputArray),
+                            Arrays.toString(expectedResult),
+                            Arrays.toString(actualResult)));
+        }
+    }
+}
