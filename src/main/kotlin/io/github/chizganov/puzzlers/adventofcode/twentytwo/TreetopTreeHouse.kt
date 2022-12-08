195c67f0029d0e734b823d078c2033d0554a73b6
Ev Chizganov <evchizganov@gmail.com>
2022-12-09 01:18:52 +0200

    Refactor Advent Of Code Day 8

--- src/main/kotlin/io/github/chizganov/puzzlers/adventofcode/twentytwo/TreetopTreeHouse.kt
+++ src/main/kotlin/io/github/chizganov/puzzlers/adventofcode/twentytwo/TreetopTreeHouse.kt
@@ -6,82 +6,60 @@ import kotlin.math.max
  * Advent of Code 2022 Day 8: Treetop Tree House
  * https://adventofcode.com/2022/day/8
  * */
-class TreetopTreeHouse {
+open class TreetopTreeHouse {
 
-    fun findVisibleTreesNumber(gridMap: List<String>): Int {
-        val grid = gridMap.map { it.toCharArray().map(Char::digitToInt) }
-        var visibleNumber = grid.size * 2 + grid.first().size * 2 - 4 // minus duplicate corners
-        for (row in 1 until grid.lastIndex) {
-            column@ for (column in 1 until grid[row].lastIndex) {
-                // check in row left
-                for (i in column - 1 downTo 0) {
-                    if (grid[row][i] >= grid[row][column]) break
-                    if (grid[row][i] < grid[row][column] && i == 0) {
-                        visibleNumber++
-                        continue@column
-                    }
-                }
-                // check in row right
-                for (i in column + 1..grid[row].lastIndex) {
-                    if (grid[row][i] >= grid[row][column]) break
-                    if (grid[row][i] < grid[row][column] && i == grid[row].lastIndex) {
-                        visibleNumber++
-                        continue@column
-                    }
-                }
-                // check in column up
-                for (i in row - 1 downTo 0) {
-                    if (grid[i][column] >= grid[row][column]) break
-                    if (grid[i][column] < grid[row][column] && i == 0) {
-                        visibleNumber++
-                        continue@column
-                    }
-                }
-                // check in column down
-                for (i in row + 1..grid.lastIndex) {
-                    if (grid[i][column] >= grid[row][column]) break
-                    if (grid[i][column] < grid[row][column] && i == grid.lastIndex) {
-                        visibleNumber++
-                        continue@column
-                    }
+    fun findVisibleTreesNumber(inputMap: List<String>): Int {
+        val treeMap = inputMap.map { it.toCharArray().map(Char::digitToInt) }
+
+        var visibleNumber = treeMap.size * 2 + treeMap.first().size * 2 - 4 // minus duplicate corners
+        for (row in 1 until treeMap.lastIndex) {
+            for (column in 1 until treeMap[row].lastIndex) {
+                if (isVisibleFromEdge(treeMap, row, column, -1, 0) ||
+                    isVisibleFromEdge(treeMap, row, column, 1, 0) ||
+                    isVisibleFromEdge(treeMap, row, column, 0, -1) ||
+                    isVisibleFromEdge(treeMap, row, column, 0, 1)
+                ) {
+                    visibleNumber++
                 }
             }
         }
+
         return visibleNumber
     }
 
-    fun findHighestScenicScore(gridMap: List<String>): Int {
-        val grid = gridMap.map { it.toCharArray().map(Char::digitToInt) }
+    private fun isVisibleFromEdge(grid: List<List<Int>>, row: Int, column: Int, dx: Int, dy: Int): Boolean {
+        var curRow = row + dy
+        var curColumn = column + dx
+
+        while (curRow >= 0 && curRow < grid.size && curColumn >= 0 && curColumn < grid[curRow].size) {
+            if (grid[curRow][curColumn] >= grid[row][column]) {
+                return false
+            }
+            curRow += dy
+            curColumn += dx
+        }
+
+        return grid[curRow - dy][curColumn - dx] < grid[row][column]
+    }
+
+    fun findHighestScenicScore(inputMap: List<String>): Int {
+        val treeMap = inputMap.map { it.toCharArray().map(Char::digitToInt) }
+
         var maxScenicScore = 0
-        for (row in 1 until grid.lastIndex) {
-            for (column in 1 until grid[row].lastIndex) {
-                // check in row left
-                var leftScore = 0
-                for (i in column - 1 downTo 0) {
-                    leftScore++
-                    if (grid[row][i] >= grid[row][column]) break
-                }
-                // check in row right
-                var rightScore = 0
-                for (i in column + 1..grid[row].lastIndex) {
-                    rightScore++
-                    if (grid[row][i] >= grid[row][column]) break
-                }
-                // check in column up
-                var upScore = 0
-                for (i in row - 1 downTo 0) {
-                    upScore++
-                    if (grid[i][column] >= grid[row][column]) break
-                }
-                // check in column down
-                var downScore = 0
-                for (i in row + 1..grid.lastIndex) {
-                    downScore++
-                    if (grid[i][column] >= grid[row][column]) break
-                }
-                maxScenicScore = max(maxScenicScore, leftScore * rightScore * upScore * downScore)
+        for (row in 1 until treeMap.lastIndex) {
+            for (col in 1 until treeMap[row].lastIndex) {
+                val left = (col - 1 downTo 1)
+                    .takeWhile { treeMap[row][it] < treeMap[row][col] }.count() + 1
+                val right = (col + 1 until treeMap[row].lastIndex)
+                    .takeWhile { treeMap[row][it] < treeMap[row][col] }.count() + 1
+                val up = (row - 1 downTo 1)
+                    .takeWhile { treeMap[it][col] < treeMap[row][col] }.count() + 1
+                val down = (row + 1 until treeMap.lastIndex)
+                    .takeWhile { treeMap[it][col] < treeMap[row][col] }.count() + 1
+                maxScenicScore = max(maxScenicScore, left * right * up * down)
             }
         }
+
         return maxScenicScore
     }
 }
