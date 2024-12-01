cdfb9edc8666c10dfe0e5cd16d76a62ab905dd40
Ev Chizganov <evchizganov@gmail.com>
2024-12-01 21:38:35 +0200

    Refresh dependencies
    Took 15 minutes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.21"
+    kotlin("jvm") version "2.1.0"
 }
 
 group = "io.github.chizganov"
@@ -13,8 +14,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
-    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testImplementation(platform("org.junit:junit-bom:5.11.3"))
     testImplementation("org.junit.jupiter:junit-jupiter-params")
 }
 
@@ -22,6 +23,14 @@ tasks.test {
     useJUnitPlatform()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
+    }
 }
